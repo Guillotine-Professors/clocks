38396441ec99990c0ce8373ba603fd72a060914f
Guillotine University Professor Union <guillotine.univ@protonmail.ch>
2025-03-24 14:06:39 +0900

    make downflap clock

--- lib/flapclock/flaptile.tsx
+++ lib/flapclock/flaptile.tsx
@@ -6,15 +6,19 @@ import { FlapDisplay } from 'react-split-flap-effect'
 import 'react-split-flap-effect/extras/themes.css'
 import css from "./flaptile.module.css"
 
-function FlapTile(value:number, length?:number){
+function FlapTile(value:number, length?:number, up?:boolean){
     return <FlapDisplay 
     className={`L ${css.inblock}`}
-    chars={'1234567890 '}
+    chars={up==true||up==undefined?'1234567890 ':'0987654321 '}
     value={String(value)}
     length={value>1000?undefined:(length??3)}
     padMode={"start"}
     style={{display: "inline-block"}}
   />
+}
+
+export function FlapTileDown(value:number, length?:number){
+  return FlapTile(value, length, false)
 }
 
 export default FlapTile;
