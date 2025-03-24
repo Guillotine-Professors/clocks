06b4cf3aaa30391000db3cce7a7feb4434b3457a
Guillotine University Professor Union <guillotine.univ@protonmail.ch>
2025-03-24 14:06:59 +0900

    make downflap clock and description of tile

--- lib/clocks/diffclocktile.tsx
+++ lib/clocks/diffclocktile.tsx
@@ -1,13 +1,13 @@
 import Container from "react-bootstrap/Container";
 import FlapDiffClock from "./diffclock";
 
-function FlapDiffClockTile(props:{ntime:Date, otime:Date, msgTop?:string, msgBottom?:string, msgleft?:string, msgright?:string}){
+function FlapDiffClockTile(props:{ntime:Date, otime:Date, msgTop?:string, msgBottom?:string, msgleft?:string, msgright?:string, up?:boolean, desc?:string}){
 
     return <Container>
         {props.msgTop?<><h2>{props.msgTop}</h2></>:<></>}
-        {props.msgleft?<>{props.msgTop}</>:<></>}
-        <FlapDiffClock ntime={props.ntime} otime={props.otime} msgright={props.msgright} msgleft={props.msgleft}/>
+        <FlapDiffClock ntime={props.ntime} otime={props.otime} msgright={props.msgright} msgleft={props.msgleft} up={props.up}/>
         {props.msgBottom?<><br/><h2>{props.msgBottom}</h2></>:<></>}
+        {props.desc?<p>{props.desc}</p>:<></>}
     </Container>
 
 }
