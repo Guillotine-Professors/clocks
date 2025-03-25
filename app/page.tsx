7a1ffafd558f959c258f55129379e91832d148ec
Guillotine University Professor Union <guillotine.univ@protonmail.ch>
2025-03-25 11:35:17 +0900

    add judge of ccourt

--- app/page.tsx
+++ app/page.tsx
@@ -30,6 +30,11 @@ export default function Home() {
       </Container>
 
       <Container style={{margin:0, marginTop:1+"em", padding:0, borderWidth:0.5+'pt', border:'solid', borderRadius:1+'em'}}>
+      <FlapDiffClockTile ntime={ntime} otime={new Date("2025-04-18T23:59:59.999+09:00")}
+      msgTop="문형배, 이미선 헌법재판소 재판관의 임기가" msgright="남았습니다." up={false}/>
+      </Container>
+
+      <Container style={{margin:0, marginTop:1+"em", padding:0, borderWidth:0.5+'pt', border:'solid', borderRadius:1+'em'}}>
       <FlapDiffClockTile ntime={ntime} otime={new Date("2025-02-13T05:00:00.000+09:00")}
       msgTop="고진수 서비스연맹 관광레저산업노조 세종호텔지부장이 세종호텔 앞 지하차도 안내 구조물에서 고공농성을 한 지" msgright="지났습니다."/>
       </Container>
